b97860f55103d1f5c50c4cc9444e1d3f0a7816bc
Juan Farias <dientuki@gmail.com>
2016-11-12 17:24:53 -0300

    added some seeders

--- app/User.php
+++ app/User.php
@@ -26,4 +26,12 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public function setPasswordAttribute($value)
+    {
+        if ( ! empty ($value))
+        {
+            $this->attributes['password'] = bcrypt($value);
+        }
+    }
 }
---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => 'Atyca',
+            'email' => 'info@atyca.tur.ar',
+            'password' => bcrypt('admin'),
+        ]);
+    }
+}
