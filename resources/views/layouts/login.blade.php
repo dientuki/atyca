4426f5470e65cf9f0d8ac8e50d1ee5915ac73677
Juan Farias <dientuki@gmail.com>
2016-11-13 16:31:38 -0300

    fix some shit

---
+++ resources/views/layouts/login.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="es">
+  <head>
+    @include ('main-parts/head')
+  </head>
+
+  <body class="login-page">
+    @include ('main-parts/header')
+
+    <div class="main-content">
+      <main id="main">
+        <div class="main-wrapper clearfix">
+          @yield('content')
+        </div>
+      </main>
+
+    </div>
+
+    @include ('main-parts/footer')
+  	@include ('main-parts/script')
+  	
+  </body>
+
+</html>
