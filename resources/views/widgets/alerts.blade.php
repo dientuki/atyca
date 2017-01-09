fe620b83e0ffdde7a41fa6664376aac55bbdfaf2
Juan Farias <dientuki@gmail.com>
2017-01-09 16:17:02 -0300

    fix alert bug

--- resources/views/widgets/alerts.blade.php
+++ resources/views/widgets/alerts.blade.php
@@ -1,34 +1,11 @@
 @foreach (Alert::getMessages() as $type => $messages)
   <div class="alert-wrapper">
     @foreach ($messages as $message)
-      <div class="alert alert-{{ $type }}"><strong>{{ ucfirst(trans('alerts.' . $type)) }}!</strong> {{ $message }} <span class="alert-close"></span></div>
+      <div class="alert-animation animated zoomIn">
+        <div class="alert-fix">
+          <div class="alert alert-{{ $type }}"><strong>{{ ucfirst(trans('alerts.' . $type)) }}!</strong> {{ $message }} <span class="alert-close"></span></div>
+        </div>
+      </div>
     @endforeach
   </div>
 @endforeach
-
-<div class="alert-wrapper">
-  <div class="alert-animation animated zoomIn">
-    <div class="alert-fix">
-      <div class="alert alert-success"><strong>success!</strong> el mensaje es claro <span class="alert-close"></span></div>
-    </div>
-  </div>
-  <div class="alert-animation animated zoomIn">
-    <div class="alert-fix">
-      <div class="alert alert-success"><strong>success!</strong> el mensaje es claro <span class="alert-close"></span></div>
-    </div>
-  </div>
-  <div class="alert-animation animated zoomIn">
-    <div class="alert-fix">
-      <div class="alert alert-success"><strong>success!</strong> el mensaje es claro <span class="alert-close"></span></div>
-    </div>
-  </div>
-  <div class="alert-animation animated zoomIn">
-    <div class="alert-fix">
-      <div class="alert alert-success"><strong>success!</strong> el mensaje es claro <span class="alert-close"></span></div>
-    </div>
-  </div>
-
-
-
-
-</div>
