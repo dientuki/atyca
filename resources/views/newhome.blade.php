bd0efaca028c0a0896c96f5de3ed3776df286121
Juan Farias <dientuki@gmail.com>
2017-05-12 10:40:08 -0300

    added html new home

--- resources/views/newhome.blade.php
+++ resources/views/newhome.blade.php
@@ -2,13 +2,24 @@
 
 @section ('content')
 
-  <div class="block-aside-left">
+  <div class="block-aside-left country-selector">
+    <div class="country-selector-title">Clientes de Argentina</div>
 
+    <a class="button-submit" href="#">Ingresar</a>
   </div>
 
 
-  <div class="block-main-right">
-    
+  <div class="block-main-right country-selector">
+    <div class="country-selector-title">Clientes del Exterior</div>
+
+    <?php $countries = array('Colombia','Brasil','Chile','Uruguay','Guatemala','EE.UU','Bolivia','Costa Rica','Paraguay', 'República Dominicana', 'Venezuela', 'Ecuador', 'Perú', 'Aruba', 'Panama', 'Canada'); ?>
+
+    <ul class="country-list">
+      @foreach ($countries as $country)
+        <li class="country-list-item country-{{ str_slug($country)  }}">{{$country}}</li>
+      @endforeach
+    </ul>
+    <a class="button-submit" href="#">Ingresar</a>
   </div>
 
 @endsection
