c696fbc42b32a689bfbdf552c8297ad4b1bff137
Juan Farias <dientuki@gmail.com>
2017-05-15 10:27:14 -0300

    fix footer

---
+++ resources/views/main-parts/newfooter.blade.php
@@ -0,0 +1,51 @@
+<footer>
+
+  <div class="gob-medals">
+    <div class="main-wrapper">
+      <ul class="gob-medals-container clearfix">
+        <li class="gob-medals-item">
+          <img alt="Patagonia Argentina" class="afterload" data-original="{{ asset('images/logos/patagonia-argentina.png') }}" height="60" src="data:image/gif;base64,R0lGODlhAQABAIAAAP///////yH5BAEHAAEALAAAAAABAAEAAAICTAEAOw==" title="Patagonia Argentina" width="192">
+        </li>
+        <li class="gob-medals-item">
+          <img alt="Norte Argentina" class="afterload" data-original="{{ asset('images/logos/norte-argentina.png') }}" height="60" src="data:image/gif;base64,R0lGODlhAQABAIAAAP///////yH5BAEHAAEALAAAAAABAAEAAAICTAEAOw==" title="Norte Argentina" width="113">
+        </li>
+        <li class="gob-medals-item">
+          <img alt="Cordoba" class="afterload" data-original="{{ asset('images/logos/cordoba.png') }}" height="60" src="data:image/gif;base64,R0lGODlhAQABAIAAAP///////yH5BAEHAAEALAAAAAABAAEAAAICTAEAOw==" title="Cordoba" width="123">
+        </li>
+        <li class="gob-medals-item">
+          <img alt="Turismo Buenos Aires" class="afterload" data-original="{{ asset('images/logos/turismo-buenos-aires.png') }}" height="60" src="data:image/gif;base64,R0lGODlhAQABAIAAAP///////yH5BAEHAAEALAAAAAABAAEAAAICTAEAOw==" title="Turismo Buenos Aires" width="110">
+        </li>
+        <li class="gob-medals-item">
+          <img alt="Cuyo Argentina" class="afterload" data-original="{{ asset('images/logos/cuyo-argentina.png') }}" height="60" src="data:image/gif;base64,R0lGODlhAQABAIAAAP///////yH5BAEHAAEALAAAAAABAAEAAAICTAEAOw==" title="Cuyo Argentina" width="113">
+        </li>
+        <li class="gob-medals-item qr-code">
+          <img alt="Litoral Argentina" class="afterload" data-original="{{ asset('images/logos/litoral-argentina.png') }}" height="60" src="data:image/gif;base64,R0lGODlhAQABAIAAAP///////yH5BAEHAAEALAAAAAABAAEAAAICTAEAOw==" title="Litoral Argentina" width="137">
+        </li>
+      </ul>
+    </div>
+
+  </div>
+
+  <div id="footer" class="footer">
+
+    <section class="main-wrapper">
+      <h2 class="footer-title">Atyca – Operador Mayorista</h2>
+      <p>
+        Atyca Viajes S.R.L.
+        <span class="footer-highlight">Cuit 30-71026611-1 | Legajo 13684</span>
+        Dirección:
+        <span class="footer-highlight">Tucumán Nº 1553 Piso 2 Of. “D” C1050AAE</span>
+        Email:
+        <a class="footer-highlight" href="mailto:info@atyca.tur.ar" title="info@atyca.tur.ar">info@atyca.tur.ar</a>
+      </p>
+      <p>
+        Tel: (Lu. a Vi. 10:00hs a 19:00hs.):
+        <span class="footer-highlighta" title="Tel: (Lu. a Vi. 10:00hs a 19:00hs.)">+54-11-4371-187</span>
+        Móvil (Fuera de horario comercial):
+        <a class="footer-highlight" title="Móvil (Fuera de horario comercial)">+54-9-11-5452-8187</a>
+        <a class="footer-highlight" title="Móvil (Fuera de horario comercial)">+54-9-11-6480-6298</a>
+        <a class="footer-highlight" title="Móvil (Fuera de horario comercial)">+54-9-11-5479-9718</a>
+      </p>
+    </section>
+  </div>
+</footer>
