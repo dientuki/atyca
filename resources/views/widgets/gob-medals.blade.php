17fe84f159566b8e0c446fb9bd95e0c4654af9a5
Juan Farias <dientuki@gmail.com>
2017-06-24 23:12:12 -0300

    fix footer

--- resources/views/widgets/gob-medals.blade.php
+++ resources/views/widgets/gob-medals.blade.php
@@ -13,8 +13,8 @@
       <li class="gob-medals-item">
         <img alt="Ministerio de Turismo" class="afterload" data-original="{{ asset('images/logos/ministerio-turismo.png') }}" height="62" src="data:image/gif;base64,R0lGODlhAQABAIAAAP///////yH5BAEHAAEALAAAAAABAAEAAAICTAEAOw==" title="Ministerio de Turismo" width="212">
       </li>
-      <li class="gob-medals-item">
-        <img alt="Cuyo Argentina" class="afterload" data-original="{{ asset('images/logos/cuyo-argentina.png') }}" height="60" src="data:image/gif;base64,R0lGODlhAQABAIAAAP///////yH5BAEHAAEALAAAAAABAAEAAAICTAEAOw==" title="Cuyo Argentina" width="113">
+      <li class="gob-medals-item qr-code-tur">
+        <img alt="Data Fiscal" class="afterload" data-original="{{ asset('images/logos/qr-tur.png') }}" height="152" src="data:image/gif;base64,R0lGODlhAQABAIAAAP///////yH5BAEHAAEALAAAAAABAAEAAAICTAEAOw==" title="Data Fiscal" width="111">
       </li>
       <li class="gob-medals-item qr-code">
         <img alt="Data Fiscal" class="afterload" data-original="{{ asset('images/logos/afip.png') }}" height="152" src="data:image/gif;base64,R0lGODlhAQABAIAAAP///////yH5BAEHAAEALAAAAAABAAEAAAICTAEAOw==" title="Data Fiscal" width="111">
