9a9e24cb907564446dfef822dd1c7b42da839136
Juan Farias <dientuki@gmail.com>
2017-06-24 21:57:42 -0300

    Revert "fix padding in tactic"
    This reverts commit 4d82a3029426ae38db187a2b6fa27deb5085cb25.

--- resources/views/widgets/tactic-2.blade.php
+++ resources/views/widgets/tactic-2.blade.php
@@ -1,4 +1,4 @@
-<div class="tactic tactic-no-padding">
+<div class="tactic">
   <div class="main-wrapper clearfix">
     <div class="tactic-item Wallop Wallop-slide-SmallArrow">
       <div class="Wallop-list">
