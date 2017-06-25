d73ae7dc542846d36ae8f2738299193044b5b717
Juan Farias <dientuki@gmail.com>
2017-06-25 00:32:23 -0300

    added more pages

---
+++ resources/views/static/salidas-grupales.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.site')
+
+@section ('content')
+
+  <article class="entry block-main-left">
+    <h1 class="entry-title">La empresa</h1>
+    <div class="entry-content">
+      <p>
+        Atyca es una empresa de viajes y turismo integrada por un equipo de profesionales con amplia trayectoria, conocimiento y experiencia en el mundo del turismo. Cuenta con la infraestructura y la tecnología adecuada para responder a las exigencias de sus deseos de manera ágil y efectiva; asumiendo el compromiso de brindar todo su conocimiento profesional.
+      </p>
+      <p>
+        Modernas oficinas emplazadas en el corazón de la Ciudad de Buenos Aires; innovación permanente; creatividad y profesionalidad de sus agentes; y orientación destinada a satisfacer cada una de sus necesidades se plasman día a día en la forma de trabajar en Atyca, persiguiendo siempre el objetivo de marcar la diferencia en todo momento.
+      </p>
+    </div>
+  </article>
+  <div class="block-aside-right deco-box">
+    <img>
+  </div>
+
+@endsection
