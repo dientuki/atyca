153614b73a9bd42b9fe66c651023aa46830019bf
Juan Farias <dientuki@gmail.com>
2017-06-24 23:26:27 -0300

    fix in links

--- resources/views/widgets/gob-medals.blade.php
+++ resources/views/widgets/gob-medals.blade.php
@@ -14,10 +14,10 @@
         <img alt="Ministerio de Turismo" class="afterload" data-original="{{ asset('images/logos/ministerio-turismo.png') }}" height="62" src="data:image/gif;base64,R0lGODlhAQABAIAAAP///////yH5BAEHAAEALAAAAAABAAEAAAICTAEAOw==" title="Ministerio de Turismo" width="212">
       </li>
       <li class="gob-medals-item qr-code-tur">
-        <img alt="Data Fiscal" class="afterload" data-original="{{ asset('images/logos/qr-tur.png') }}" height="152" src="data:image/gif;base64,R0lGODlhAQABAIAAAP///////yH5BAEHAAEALAAAAAABAAEAAAICTAEAOw==" title="Data Fiscal" width="111">
+        <a target="_blank" href="http://servicios.turismo.gob.ar/agencias/constancia/13684"><img alt="Ministerio de turismo" class="afterload" data-original="{{ asset('images/logos/qr-tur.png') }}" height="152" src="data:image/gif;base64,R0lGODlhAQABAIAAAP///////yH5BAEHAAEALAAAAAABAAEAAAICTAEAOw==" title="Ministerio de turismo" width="111"></a>
       </li>
       <li class="gob-medals-item qr-code">
-        <img alt="Data Fiscal" class="afterload" data-original="{{ asset('images/logos/afip.png') }}" height="152" src="data:image/gif;base64,R0lGODlhAQABAIAAAP///////yH5BAEHAAEALAAAAAABAAEAAAICTAEAOw==" title="Data Fiscal" width="111">
+        <a target="_blank" href="https://serviciosweb.afip.gob.ar/clavefiscal/qr/publicInfoD.aspx"><img alt="Data Fiscal" class="afterload" data-original="{{ asset('images/logos/afip.png') }}" height="152" src="data:image/gif;base64,R0lGODlhAQABAIAAAP///////yH5BAEHAAEALAAAAAABAAEAAAICTAEAOw==" title="Data Fiscal" width="111"></a>
       </li>
     </ul>
   </div>
