e98bb471350e48c0de51fba61cb1ffb29c56a588
Juan Farias <dientuki@gmail.com>
2017-06-28 01:24:34 -0300

    added banner

--- resources/views/widgets/tactic-1-new.blade.php
+++ resources/views/widgets/tactic-1-new.blade.php
@@ -3,11 +3,9 @@
     <div class="tactic-item ">
       <div >
                <a  href="https://www.parquesnacionales.gob.ar/" target="_blank">
-
-
-<video autoplay preload>
-  <source src="{{ asset('media/parques.mp4') }}" type="video/mp4">
-</video>
+                <video autoplay preload width="301" height="169">
+                  <source src="{{ asset('media/parques.mp4') }}" type="video/mp4">
+                </video>
                </a>
 
       </div>
@@ -17,7 +15,6 @@
         <a  href="http://www.aa2000.com.ar/" target="_blank">
           <img src="{{ asset('images/tactic/arribos.png') }}">
         </a>
-       
       </div>
     </div>
   <div class="tactic-item ">
@@ -25,7 +22,6 @@
         <a  href="http://www.migraciones.gov.ar/accesible/indexP.php?tramitesindex" target="_blank">
           <img src="{{ asset('images/tactic/migraciones.png') }}">
         </a>
-       
       </div>
     </div>
   </div>
