a1602a1c8ed474991f53369e355413592dbbc803
Juan Farias <dientuki@gmail.com>
2017-06-28 01:03:55 -0300

    fix widgets by dvd

---
+++ resources/views/widgets/tactic-1-new.blade.php
@@ -0,0 +1,33 @@
+<div class="tactic">
+  <div class="main-wrapper clearfix">
+    <div class="tactic-item ">
+      <div >
+               <a  href="https://www.parquesnacionales.gob.ar/" target="_blank">
+
+
+<video autoplay preload>
+  <source src="{{ asset('media/parques.mp4') }}" type="video/mp4">
+</video>
+               </a>
+
+      </div>
+    </div>
+    <div class="tactic-item ">
+      <div >
+        <a  href="http://www.aa2000.com.ar/" target="_blank">
+          <img src="{{ asset('images/tactic/arribos.png') }}">
+        </a>
+       
+      </div>
+    </div>
+  <div class="tactic-item ">
+      <div >
+        <a  href="http://www.migraciones.gov.ar/accesible/indexP.php?tramitesindex" target="_blank">
+          <img src="{{ asset('images/tactic/migraciones.png') }}">
+        </a>
+       
+      </div>
+    </div>
+  </div>
+
+</div>
---
+++ resources/views/widgets/tactic.blade.php
@@ -0,0 +1,50 @@
+<div class="tactic" id="tactic">
+  <div class="main-wrapper clearfix">
+    <div class="tactic-item Wallop Wallop-slide-SmallArrow">
+      <div class="Wallop-list">
+        <a class="Wallop-item" href="http://www.argenta-tower.com.ar/" target="_blank">
+          <img src="{{ asset('images/tactic/argenta.png') }}">
+        </a>
+        <a class="Wallop-item" href="http://www.amerian.com/hotel-merit-san-telmo-hotel" target="_blank">
+          <img src="{{ asset('images/tactic/merit.png') }}">
+        </a>
+        <a class="Wallop-item" href="http://www.alvearpalace.com/" target="_blank">
+          <img src="{{ asset('images/tactic/alvear.png') }}">
+        </a>
+      </div>
+      <span class="Wallop-ArrowPrevious">Anterior</span>
+      <span class="Wallop-ArrowNext">Siguiente</span>
+    </div>
+    <div class="tactic-item Wallop Wallop-slide-SmallArrow">
+      <div class="Wallop-list">
+        <a class="Wallop-item" href="http://tangoporteno.com.ar/" target="_blank">
+          <img src="{{ asset('images/tactic/porteno.png') }}">
+        </a>
+        <a class="Wallop-item" href="http://www.laventanaweb.com/" target="_blank">
+          <img src="{{ asset('images/tactic/ventana.png') }}">
+        </a>
+        <a class="Wallop-item" href="http://www.rojotango.com/" target="_blank">
+          <img src="{{ asset('images/tactic/rojo.png') }}">
+        </a>
+      </div>
+      <span class="Wallop-ArrowPrevious">Anterior</span>
+      <span class="Wallop-ArrowNext">Siguiente</span>
+    </div>
+    <div class="tactic-item Wallop Wallop-slide-SmallArrow">
+      <div class="Wallop-list">
+        <a class="Wallop-item" href="http://www.hotelinacayal.com.ar/" target="_blank">
+          <img src="{{ asset('images/tactic/cacique.png') }}">
+        </a>
+        <a class="Wallop-item" href="http://www.miradordellago.com.ar/" target="_blank">
+          <img src="{{ asset('images/tactic/mirador.png') }}">
+        </a>
+        <a class="Wallop-item" href="http://iguazu.grandcrucero.com/" target="_blank">
+          <img src="{{ asset('images/tactic/crucero.png') }}">
+        </a>
+      </div>
+      <span class="Wallop-ArrowPrevious">Anterior</span>
+      <span class="Wallop-ArrowNext">Siguiente</span>
+    </div>
+  </div>
+
+</div>
