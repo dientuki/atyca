8386d0612cadfba7ee07ef8245a046b025e33f83
Juan Farias <dientuki@gmail.com>
2017-07-08 16:23:10 -0300

    fix url data fiscal

--- resources/views/widgets/gob-medals.blade.php
+++ resources/views/widgets/gob-medals.blade.php
@@ -2,22 +2,26 @@
   <div class="main-wrapper">
     <ul class="gob-medals-container clearfix">
       <li class="gob-medals-item">
-        <img alt="Argentina" class="afterload" data-original="{{ asset('images/logos/marca-pais.png') }}" height="55" src="data:image/gif;base64,R0lGODlhAQABAIAAAP///////yH5BAEHAAEALAAAAAABAAEAAAICTAEAOw==" title="Argentina" width="117">
+        <img alt="Argentina" class="afterload" data-original="{{ asset('images/logos/marca-pais.png') }}" height="55" src="data:image/gif;base64,R0lGODlhAQABAIAAAP///////yH5BAEHAAEALAAAAAABAAEAAAICTAEAOw==" title="Argentina" width="117" />
       </li>
       <li class="gob-medals-item">
-        <img alt="Tur.ar" class="afterload" data-original="{{ asset('images/logos/turar.png') }}" height="60" src="data:image/gif;base64,R0lGODlhAQABAIAAAP///////yH5BAEHAAEALAAAAAABAAEAAAICTAEAOw==" title="Tur.ar" width="129">
+        <img alt="Tur.ar" class="afterload" data-original="{{ asset('images/logos/turar.png') }}" height="60" src="data:image/gif;base64,R0lGODlhAQABAIAAAP///////yH5BAEHAAEALAAAAAABAAEAAAICTAEAOw==" title="Tur.ar" width="129" />
       </li>
       <li class="gob-medals-item">
-        <img alt="Aviabue" class="afterload" data-original="{{ asset('images/logos/aviabue.png') }}" height="59" src="data:image/gif;base64,R0lGODlhAQABAIAAAP///////yH5BAEHAAEALAAAAAABAAEAAAICTAEAOw==" title="Aviabue" width="56">
+        <img alt="Aviabue" class="afterload" data-original="{{ asset('images/logos/aviabue.png') }}" height="59" src="data:image/gif;base64,R0lGODlhAQABAIAAAP///////yH5BAEHAAEALAAAAAABAAEAAAICTAEAOw==" title="Aviabue" width="56" />
       </li>
       <li class="gob-medals-item">
-        <img alt="Ministerio de Turismo" class="afterload" data-original="{{ asset('images/logos/ministerio-turismo.png') }}" height="62" src="data:image/gif;base64,R0lGODlhAQABAIAAAP///////yH5BAEHAAEALAAAAAABAAEAAAICTAEAOw==" title="Ministerio de Turismo" width="212">
+        <img alt="Ministerio de Turismo" class="afterload" data-original="{{ asset('images/logos/ministerio-turismo.png') }}" height="62" src="data:image/gif;base64,R0lGODlhAQABAIAAAP///////yH5BAEHAAEALAAAAAABAAEAAAICTAEAOw==" title="Ministerio de Turismo" width="212" />
       </li>
       <li class="gob-medals-item qr-code-tur">
-        <a target="_blank" href="http://servicios.turismo.gob.ar/agencias/constancia/13684"><img alt="Ministerio de turismo" class="afterload" data-original="{{ asset('images/logos/qr-tur.png') }}" height="152" src="data:image/gif;base64,R0lGODlhAQABAIAAAP///////yH5BAEHAAEALAAAAAABAAEAAAICTAEAOw==" title="Ministerio de turismo" width="111"></a>
+        <a target="_blank" href="http://servicios.turismo.gob.ar/agencias/constancia/13684">
+          <img alt="Ministerio de turismo" class="afterload" data-original="{{ asset('images/logos/qr-tur.png') }}" height="152" src="data:image/gif;base64,R0lGODlhAQABAIAAAP///////yH5BAEHAAEALAAAAAABAAEAAAICTAEAOw==" title="Ministerio de turismo" width="111" />
+        </a>
       </li>
       <li class="gob-medals-item qr-code">
-        <a target="_blank" href="https://serviciosweb.afip.gob.ar/clavefiscal/qr/publicInfoD.aspx"><img alt="Data Fiscal" class="afterload" data-original="{{ asset('images/logos/afip.png') }}" height="152" src="data:image/gif;base64,R0lGODlhAQABAIAAAP///////yH5BAEHAAEALAAAAAABAAEAAAICTAEAOw==" title="Data Fiscal" width="111"></a>
+        <a target="_blank" href="http://qr.afip.gob.ar/?qr=EdFg14Zu_DJapdI5Y92rTQ,,">
+          <img alt="Data Fiscal" class="afterload" data-original="{{ asset('images/logos/afip.png') }}" height="152" src="data:image/gif;base64,R0lGODlhAQABAIAAAP///////yH5BAEHAAEALAAAAAABAAEAAAICTAEAOw==" title="Data Fiscal" width="111" />
+        </a>
       </li>
     </ul>
   </div>
