bb001e09e9ee8029e13ebe3e2d708b8f1f9cc3e3
Juan Farias <dientuki@gmail.com>
2018-07-26 11:50:29 -0300

    fix destinos general

--- resources/views/static/destinos-general.blade.php
+++ resources/views/static/destinos-general.blade.php
@@ -1,19 +1,27 @@
-@extends('layouts.ads-site')
+@extends('layouts.site')
 
 @section ('content')
 
-  <article class="entry ">
-    <h1 class="entry-title">Destinos</h1>
-    <div class="entry-content">
-      <p>
-        
+  <?php $destinations = array('Argentina','Brasil','Canada','Chile','Ecuador','EEUU','Guatemala','Mexico','República Dominicana','Uruguay','Venezuela'); ?>
 
+  <section class="destinations">
+    <h6 class="visuallyhidden">Destinos</h6>
+    <ul class="destinations-list clearfix">
 
-        
-      </p>
-    
-    </div>
-  </article>
+      @foreach ($destinations as $destination)
+
+        <?php $slug = str_slug($destination); ?>
+        <li class="destinations-country">
+          <a class="destinations-link" href="{{ route('destinations.show', ['slug' => $slug]) }}" title="{{ $destination  }}">
+            <img alt="{{ $destination  }}" class="afterload destinations-flag" data-original="{{ asset('images/destinations-general/'.$slug.'.png') }}" src="data:image/gif;base64,R0lGODlhAQABAIAAAP///////yH5BAEHAAEALAAAAAABAAEAAAICTAEAOw==" title="{{ $destination  }}">
+            <span class="destinations-text">{{ $destination  }}</span>
+          </a>
+        </li>
+
+      @endforeach
+
+    </ul>
+  </section>
 
 
 @endsection
