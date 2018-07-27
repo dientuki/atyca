33071a8c6d46e7c1d98b7438b31ec5c82beb233f
David Santamaria <dsantamaria@knowsys.com.ar>
2018-07-28 01:28:09 +0300

    cambios banners home

--- resources/views/widgets/tactic-1-new.blade.php
+++ resources/views/widgets/tactic-1-new.blade.php
@@ -1,11 +1,10 @@
 <div class="tactic">
   <div class="main-wrapper clearfix">
     <div class="tactic-item ">
-     <a  href="https://www.parquesnacionales.gob.ar/" target="_blank">
-      <video autoplay preload width="301" height="171">
-        <source src="{{ asset('media/parques.mp4') }}" type="video/mp4">
-      </video>
-     </a>
+
+        <a  href="http://www.migraciones.gov.ar/accesible/indexP.php?tramitesindex" target="_blank">
+          <img src="{{ asset('images/tactic/migraciones.png') }}">
+        </a>
 
     </div>
     <div class="tactic-item ">
@@ -14,9 +13,14 @@
         </a>
     </div>
   <div class="tactic-item ">
-        <a  href="http://www.migraciones.gov.ar/accesible/indexP.php?tramitesindex" target="_blank">
-          <img src="{{ asset('images/tactic/migraciones.png') }}">
-        </a>
+
+    <a  href="https://www.parquesnacionales.gob.ar/" target="_blank">
+      <video autoplay preload width="301" height="171">
+        <source src="{{ asset('media/parques.mp4') }}" type="video/mp4">
+      </video>
+     </a>
+
+
     </div>
   </div>
 </div>
