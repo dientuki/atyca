a2494fc9a8bc5132ff525f33f3e8c5e3fc49e98d
Juan Farias <dientuki@gmail.com>
2019-10-10 00:39:47 -0300

    added by dvd

---
+++ resources/views/layouts/home-argentinos.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="es">
+  <head>
+    @include ('main-parts/head')
+  </head>
+
+  <body>
+    @include ('widgets/alerts')
+
+    @include ('main-parts/header')
+
+    <div class="main-content">
+      <main id="main">
+        <div class="main-wrapper clearfix">
+          @yield('content')
+        </div>
+      </main>
+
+      <aside id="aside">
+        @include ('widgets/home-argentinos')
+        @include ('widgets/logos-regiones')
+      </aside>
+    </div>
+
+
+
+    @include ('main-parts/footer')
+  	@include ('main-parts/script')
+
+  </body>
+
+</html>
---
+++ resources/views/widgets/ads-site.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="es">
+  <head>
+    @include ('main-parts/head')
+  </head>
+
+  <body>
+    @include ('widgets/alerts')
+
+    @include ('main-parts/newheader')
+
+    <div class="main-content">
+      <main id="main">
+        <div class="main-wrapper clearfix">
+          @yield('content')
+        </div>
+      </main>
+
+      <aside id="aside">
+        @include ('ads/cruceros-940x100')
+        @include ('widgets/tactic-1-new')
+      </aside>
+    </div>
+
+
+
+    @include ('main-parts/newfooter')
+  	@include ('main-parts/script')
+
+  </body>
+
+</html>
