9eb28b14c41d46e023a6a4370fe8ef1592d78531
Juan Farias <dientuki@gmail.com>
2019-10-15 23:54:22 -0300

    fix

--- resources/views/emails/contactform.blade.php
+++ resources/views/emails/contactform.blade.php
@@ -1,7 +1,9 @@
 <p>Nueva consulta enviada: {{ date('l jS \de F Y h:i A')  }}</p>
 
-<p>Empresa: {{ $data['business']}}</p>
-<p>Telefono: {{ $data['tel']}}</p>
+<p>Empresa o nombre: {{ $data['business']}}</p>
+@if (isset($data['tel']))
+  <p>Telefono: {{ $data['tel'] }}</p>
+@endif
 <p>E-mail: {{ $data['email']}}</p>
 <p>Consulta: {{ $data['message']}}</p>
 
