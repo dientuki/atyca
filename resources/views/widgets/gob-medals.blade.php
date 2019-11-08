eeb36602a101b1b9b7904d644c95b1cefcb7ce4b
Juan Farias <dientuki@gmail.com>
2019-11-08 00:24:33 -0300

    remove item

--- resources/views/widgets/gob-medals.blade.php
+++ resources/views/widgets/gob-medals.blade.php
@@ -13,11 +13,6 @@
       <li class="gob-medals-item">
         <img alt="Ministerio de Turismo" class="afterload" data-original="{{ asset('images/logos/ministerio-turismo.png') }}" height="62" src="data:image/gif;base64,R0lGODlhAQABAIAAAP///////yH5BAEHAAEALAAAAAABAAEAAAICTAEAOw==" title="Ministerio de Turismo" width="212" />
       </li>
-      <li class="gob-medals-item qr-code-tur">
-        <a target="_blank" href="http://servicios.turismo.gob.ar/agencias/constancia/13684">
-          <img alt="Ministerio de turismo" class="afterload" data-original="{{ asset('images/logos/qr-tur.png') }}" height="152" src="data:image/gif;base64,R0lGODlhAQABAIAAAP///////yH5BAEHAAEALAAAAAABAAEAAAICTAEAOw==" title="Ministerio de turismo" width="111" />
-        </a>
-      </li>
       <li class="gob-medals-item qr-code">
         <a target="_blank" href="http://qr.afip.gob.ar/?qr=EdFg14Zu_DJapdI5Y92rTQ,,">
           <img alt="Data Fiscal" class="afterload" data-original="{{ asset('images/logos/afip.png') }}" height="152" src="data:image/gif;base64,R0lGODlhAQABAIAAAP///////yH5BAEHAAEALAAAAAABAAEAAAICTAEAOw==" title="Data Fiscal" width="111" />
